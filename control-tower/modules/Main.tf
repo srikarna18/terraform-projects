6182dda58d71c2c7d18ee7d43e98f9727aa2ea81
srikarna18 <141703505+srikarna18@users.noreply.github.com>
2025-05-12 00:44:16 -0500

    Delete control-tower/modules/Main.tf

--- control-tower/modules/Main.tf
+++
@@ -1,9 +0,0 @@
-module "control_tower" {
-  source = "./modules/control_tower"
-
-  region                 = var.region
-  organization_email     = var.organization_email
-  log_archive_account_id = var.log_archive_account_id
-  audit_account_id       = var.audit_account_id
-  enable_account_factory = var.enable_account_factory
-}
