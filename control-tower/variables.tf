bcce2d6c51b340179a89827a20d4b05fe0c5a272
srikarna18 <141703505+srikarna18@users.noreply.github.com>
2025-05-12 00:49:55 -0500

    Create variables.tf

---
+++ control-tower/variables.tf
@@ -0,0 +1,7 @@
+variable "region" {}
+variable "organization_email" {}
+variable "log_archive_account_id" {}
+variable "audit_account_id" {}
+variable "enable_account_factory" {
+  default = true
+}
