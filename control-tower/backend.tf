8a88fe8458641d32a4db93e2ea0a82cdff317d14
srikarna18 <141703505+srikarna18@users.noreply.github.com>
2025-05-12 00:59:36 -0500

    Update backend.tf

--- control-tower/backend.tf
+++ control-tower/backend.tf
@@ -1,9 +1,9 @@
 terraform {
   backend "s3" {
-    bucket         = "my-prod-terraform-state"
-    key            = "control-tower/terraform.tfstate"
-    region         = "us-east-1"
-    dynamodb_table = "terraform-locks"
-    encrypt        = true
+    bucket       = "my-terraform-state-bucket"
+    key          = "path/to/my/key"
+    region       = "us-east-1"
+    encrypt      = true
+    use_lockfile = true
   }
 }
