9dd627fe9fa4f589545a8f2d5df8183fca63e5fa
srikarna18 <141703505+srikarna18@users.noreply.github.com>
2025-05-12 03:19:47 -0500

    Create main.tf

---
+++ control-tower/modules/compute/main.tf
@@ -0,0 +1,31 @@
+resource "aws_iam_role" "ec2_role" {
+  name = "ec2_ssm_role"
+  assume_role_policy = data.aws_iam_policy_document.ec2_assume.json
+}
+
+data "aws_iam_policy_document" "ec2_assume" {
+  statement {
+    actions = ["sts:AssumeRole"]
+    principals {
+      type = "Service"
+      identifiers = ["ec2.amazonaws.com"]
+    }
+  }
+}
+
+resource "aws_iam_role_policy_attachment" "ssm_attach" {
+  role       = aws_iam_role.ec2_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
+}
+
+resource "aws_instance" "web" {
+  ami           = var.ami
+  instance_type = var.instance_type
+  subnet_id     = var.subnet_id
+  iam_instance_profile = aws_iam_instance_profile.ec2_profile.name
+}
+
+resource "aws_iam_instance_profile" "ec2_profile" {
+  name = "ec2_profile"
+  role = aws_iam_role.ec2_role.name
+}
