2805f87341e6bd0689590142ed5b377c49421fa9
srikarna18 <141703505+srikarna18@users.noreply.github.com>
2025-05-12 04:54:07 -0500

    Create main.tf

---
+++ terraform-3tier-infra/modules/alb/main.tf
@@ -0,0 +1,44 @@
+resource "aws_lb" "app_alb" {
+  name               = "${var.environment}-alb"
+  internal           = false
+  load_balancer_type = "application"
+  security_groups    = [var.alb_security_group]
+  subnets            = var.public_subnet_ids
+
+  tags = {
+    Environment = var.environment
+    Name        = "${var.environment}-alb"
+  }
+}
+
+resource "aws_lb_target_group" "app_tg" {
+  name     = "${var.environment}-tg"
+  port     = 80
+  protocol = "HTTP"
+  vpc_id   = var.vpc_id
+
+  health_check {
+    path                = "/"
+    interval            = 30
+    timeout             = 5
+    healthy_threshold   = 2
+    unhealthy_threshold = 2
+    matcher             = "200"
+  }
+
+  tags = {
+    Environment = var.environment
+    Name        = "${var.environment}-tg"
+  }
+}
+
+resource "aws_lb_listener" "http" {
+  load_balancer_arn = aws_lb.app_alb.arn
+  port              = 80
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.app_tg.arn
+  }
+}
