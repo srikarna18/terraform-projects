0b76fbeeeec78b5af1ac0f0f46e4b0d4a8e1bf78
srikarna18 <141703505+srikarna18@users.noreply.github.com>
2025-05-12 05:16:54 -0500

    Update backend-setup.tf

--- terraform-3tier-infra/backend-setup.tf
+++ terraform-3tier-infra/backend-setup.tf
@@ -1,11 +1,10 @@
 
-
 provider "aws" {
   region = "us-west-2"  # Change to your AWS region
 }
 
 module "s3_backend" {
-  source = "./control-tower/modules/s3_backend_setup"  # Replace this with your actual module source path
+  source = "https://github.com/srikarna18/terraform-projects.git/control-tower/modules/s3_backend_setup"  # Replace this with your actual module source path
   
   bucket_name        = "my-tfstate-bucket"  # Replace with your desired S3 bucket name
   region             = "us-west-2"  # AWS Region
