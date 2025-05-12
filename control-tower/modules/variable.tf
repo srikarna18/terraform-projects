2288a1661a91b23e5a0d9bd3ef8233e702548e9e
srikarna18 <141703505+srikarna18@users.noreply.github.com>
2025-05-12 00:45:48 -0500

    Delete control-tower/modules/variable.tf

--- control-tower/modules/variable.tf
+++
@@ -1,7 +0,0 @@
-variable "region" {}
-variable "organization_email" {}
-variable "log_archive_account_id" {}
-variable "audit_account_id" {}
-variable "enable_account_factory" {
-  default = true
-}
