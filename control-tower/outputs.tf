adc19ae7bfd16dea33844b7743c5aaadb3b06d1c
srikarna18 <141703505+srikarna18@users.noreply.github.com>
2025-05-12 03:23:55 -0500

    Update outputs.tf

--- control-tower/outputs.tf
+++ control-tower/outputs.tf
@@ -1,3 +1,15 @@
-output "aft_account_factory_arn" {
-  value = module.control_tower.aft_account_factory_arn
+output "account_id" {
+  value = module.control_tower.account_id
+}
+
+output "vpc_id" {
+  value = module.network.vpc_id
+}
+
+output "instance_id" {
+  value = module.compute.instance_id
+}
+
+output "log_group" {
+  value = module.observability.log_group_name
 }
